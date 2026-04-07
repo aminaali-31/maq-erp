6b6bf33d4f36656213efcf6297753ac73221dba8
Amna <amina.ali.31032008@gmail.com>
2026-04-07 11:59:26 +0500

    Gave access for payroll

--- routes/hr.routes.js
+++ routes/hr.routes.js
@@ -3,20 +3,20 @@ const controller = require('../modules/hr/employee');
 const salaryController = require('../modules/hr/salary');
 const  authenticateAndAuthorize  = require('../middlewares/inventory');
 
-router.get('/dashboard',authenticateAndAuthorize([1,4]),  controller.showDashboard);
+router.get('/dashboard',authenticateAndAuthorize([1,4,3]),  controller.showDashboard);
 
-router.get('/add', authenticateAndAuthorize([1,4]),controller.showAddEmployeeForm);
-router.post('/add', authenticateAndAuthorize([1,4]),controller.addEmployee);
+router.get('/add', authenticateAndAuthorize([1,4,3]),controller.showAddEmployeeForm);
+router.post('/add', authenticateAndAuthorize([1,4,3]),controller.addEmployee);
 
-router.get('/list', authenticateAndAuthorize([1,4]),controller.listEmployees);
+router.get('/list', authenticateAndAuthorize([1,4,3]),controller.listEmployees);
 
-router.get('/payroll/list', authenticateAndAuthorize([1,4]),salaryController.listPayroll);
+router.get('/payroll/list', authenticateAndAuthorize([1,4,3]),salaryController.listPayroll);
 
-router.get('/payroll/add', authenticateAndAuthorize([1,4]),salaryController.showPayrollForm);
+router.get('/payroll/add', authenticateAndAuthorize([1,4,3]),salaryController.showPayrollForm);
 
-router.post('/payroll/add', authenticateAndAuthorize([1,4]),salaryController.addPayroll);
+router.post('/payroll/add', authenticateAndAuthorize([1,4,3]),salaryController.addPayroll);
 
-router.get('/edit/:id', authenticateAndAuthorize([1,4]), controller.editEmployeeForm);
-router.post('/edit/:id', authenticateAndAuthorize([1,4]), controller.updateEmployee);
+router.get('/edit/:id', authenticateAndAuthorize([1,4,3]), controller.editEmployeeForm);
+router.post('/edit/:id', authenticateAndAuthorize([1,4,3]), controller.updateEmployee);
 
 module.exports = router;
